850386611af8263f235c5f55ef41f79a43f4871e
akshay shinde <akshayshinde7289@gmail.com>
2022-09-12 12:44:34 +0530

    refactored sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -2,83 +2,188 @@
 import { useState } from 'react';
 import './sidebar.css';
 
+const firstMenu = {
+  visible: [
+    {
+      name: 'Home',
+      thumbnail: 'icons/home.svg'
+    },
+    {
+      name: 'Explore',
+      thumbnail: 'icons/explore.svg'
+    },
+    {
+      name: 'Shorts',
+      thumbnail: 'icons/shorts.svg'
+    },
+    {
+      name: 'Subscriptions',
+      thumbnail: 'icons/subscriptions.svg'
+    }
+  ] 
+}
+const secondMenu = {
+  visible: [
+    {
+      name: 'Library',
+      thumbnail: 'icons/library.svg'
+    },
+    {
+      name: 'History',
+      thumbnail: 'icons/history.svg'
+    },
+    {
+      name: 'Your videos',
+      thumbnail: 'icons/yourvideos.svg'
+    },
+    {
+      name: 'Watch later',
+      thumbnail: 'icons/watchlater.svg'
+    },
+    {
+      name: 'Your clips',
+      thumbnail: 'icons/yourclips.svg'
+    }
+  ],
+  hidden: [
+    {
+      name: 'Liked videos',
+      thumbnail: 'icons/liked.svg'
+    },
+    {
+      name: 'Friends | TBS',
+      thumbnail: 'icons/playlist.svg'
+    },
+    {
+      name: 'Your videos',
+      thumbnail: 'icons/playlist.svg'
+    },
+    {
+      name: 'Namaste &#128591; javascript course',
+      
+      thumbnail: 'icons/playlist.svg'
+    },
+    {
+      name: 'Photoshop for beginners',
+      thumbnail: 'icons/playlist.svg'
+    }
+  ]
+}
+const subscriptions = {
+  visible: [
+    {
+      name:'Akshay Saini',
+      thumbnail: 'subscriptions/akshay.jpg'
+    },
+    {
+      name:'Dipraj Jadhav Edits',
+      thumbnail: 'subscriptions/dipraj.jpg'
+    },
+    {
+      name:'Fireship',
+      thumbnail: 'subscriptions/fireship.jpg'
+    },
+    {
+      name:'Ishan Sharma',
+      thumbnail: 'subscriptions/ishan.jpg'
+    },
+    {
+      name:'Joma Tech',
+      thumbnail: 'subscriptions/joma.jpg'
+    }
+  ],
+  hidden: [
+    {
+      name:'Keerti Purswani',
+      thumbnail: 'subscriptions/keerti.jpg'
+    },
+    {
+      name:'Kunal Kushwaha',
+      thumbnail: 'subscriptions/kunal.jpg'
+    },
+    {
+      name:'The Net Ninja',
+      thumbnail: 'subscriptions/netninja.jpg'
+    },
+    {
+      name:'Shantanu Kumar',
+      thumbnail: 'subscriptions/shantanu.jpg'
+    },
+    {
+      name:'Finance With Sharan',
+      thumbnail: 'subscriptions/sharan.jpg'
+    },
+    {
+      name:'Tanay Pratap',
+      thumbnail: 'subscriptions/tanay.jpg'
+    }
+  ]
+}
+const explore = {
+  visible: [
+    {
+      name: 'Films',
+      thumbnail: 'icons/films.svg'
+    },
+    {
+      name: 'Gaming',
+      thumbnail: 'icons/gaming.svg'
+    },
+    {
+      name: 'Live',
+      thumbnail: 'icons/live.svg'
+    },
+    {
+      name: 'Fashion & beauty',
+      thumbnail: 'icons/fashion.svg'
+    },
+    {
+      name: 'Learning',
+      thumbnail: 'icons/learning.svg'
+    },
+    {
+      name: 'Sport',
+      thumbnail: 'icons/sport.svg'
+    }
+  ] 
+}
+const moreFromYouTube = {
+  visible: [
+    {
+      name: 'YouTube Premium',
+      thumbnail: 'icons/youtubepremium.svg'
+    },
+    {
+      name: 'Creator Studio',
+      thumbnail: 'icons/creatorstudio.svg'
+    },
+    {
+      name: 'YouTube Music',
+      thumbnail: 'icons/music.svg'
+    },
+    {
+      name: 'YouTube Kids',
+      thumbnail: 'icons/kids.svg'
+    },
+    {
+      name: 'YouTube TV',
+      thumbnail: 'icons/tv.svg'
+    }
+  ] 
+}
+
 export default function Sidebar() {
   return(
     <div className='sidebar'>
-      <div className='list first'>
-        <div>
-          <img src='icons/home.svg' alt='home' />
-          <span>Home</span>
-        </div>
-        <div>
-          <img src='icons/explore.svg' alt='explore' />
-          <span>Explore</span>
-        </div>
-        <div>
-          <img src='icons/shorts.svg' alt='shorts' />
-          <span>Shorts</span>
-        </div>
-        <div>
-          <img src='icons/subscriptions.svg' alt='subscriptions' />
-          <span>Subscriptions</span>
-        </div>
-      </div>
+      <RenderList list={firstMenu} />
       <hr />
-      <Second />
+      <RenderList list={secondMenu} />
       <hr />
-      <Subscriptions />
+      <RenderList list={subscriptions} title='SUBSCRIPTIONS' />
       <hr />
-      <div className='list explore'>
-        <p>EXPLORE</p>
-        <div>
-          <img src='icons/films.svg' alt='films' />
-          <span>Films</span>
-        </div>
-        <div>
-          <img src='icons/gaming.svg' alt='gaming' />
-          <span>Gaming</span>
-        </div>
-        <div>
-          <img src='icons/live.svg' alt='live' />
-          <span>Live</span>
-        </div>
-        <div>
-          <img src='icons/fashion.svg' alt='fashion' />
-          <span>Fashion & beauty</span>
-        </div>
-        <div>
-          <img src='icons/learning.svg' alt='learning' />
-          <span>Learning</span>
-        </div>
-        <div>
-          <img src='icons/sport.svg' alt='sport' />
-          <span>Sport</span>
-        </div>
-      </div>
+      <RenderList list={explore} title='EXPLORE' />
       <hr />
-      <div className='list more-from-youtube'>
-        <p>MORE FROM YOUTUBE</p>
-        <div>
-          <img src='icons/youtubepremium.svg' alt='youtubepremium' />
-          <span>YouTube Premium</span>
-        </div>
-        <div>
-          <img src='icons/creatorstudio.svg' alt='creatorstudio' />
-          <span>Creator Studio</span>
-        </div>
-        <div>
-          <img src='icons/music.svg' alt='music' />
-          <span>YouTube Music</span>
-        </div>
-        <div>
-          <img src='icons/kids.svg' alt='kids' />
-          <span>YouTube Kids</span>
-        </div>
-        <div>
-          <img src='icons/tv.svg' alt='tv' />
-          <span>YouTube TV</span>
-        </div>
-      </div>
+      <RenderList list={moreFromYouTube} title='MORE FROM YOUTUBE' />
       <hr />
       <div className='links'>
         <a href="#">About</a>
@@ -93,114 +198,39 @@ export default function Sidebar() {
   );
 }
 
-function Second() {
-  const [more, setMore] = useState(false);
-  return(
-    <div className='list second'>
-      <div>
-        <img src='icons/library.svg' alt='library' />
-        <span>Library</span>
-      </div>
-      <div>
-        <img src='icons/history.svg' alt='history' />
-        <span>History</span>
-      </div>
-      <div>
-        <img src='icons/yourvideos.svg' alt='yourvideos' />
-        <span>Your videos</span>
-      </div>
-      <div>
-        <img src='icons/watchlater.svg' alt='watchlater' />
-        <span>Watch later</span>
-      </div>
-      <div>
-        <img src='icons/yourclips.svg' alt='yourclips' />
-        <span>Your clips</span>
-      </div>
-      { more && 
-      <>
-        <div>
-          <img src='icons/liked.svg' alt='liked' />
-          <span>Liked videos</span>
-        </div>
-        <div>
-          <img src='icons/playlist.svg' alt='playlist' />
-          <span>Friends | TBS</span>
-        </div>
-        <div>
-          <img src='icons/playlist.svg' alt='playlist' />
-          <span>Namaste &#128591; javascript course</span>
-        </div>
-        <div>
-          <img src='icons/playlist.svg' alt='playlist' />
-          <span>Photoshop for beginners</span>
-        </div>
-      </>
-      }
-      <div onClick={() => setMore(!more)}>
-        <img src={more ? `icons/showless.svg` : 'icons/showmore.svg'} alt='showmore' />
-        <span>Show {more ? 'fewer' : 'more'}</span>
-      </div>
-    </div>
-  )
-}
-function Subscriptions() {
+function RenderList({list, title}) {
   const [more, setMore] = useState(false);
   return(
     <div className='list subscriptions'>
-      <p>SUBSCRIPTIONS</p>
-      <div>
-        <img src='subscriptions/akshay.jpg' alt='akshay' />
-        <span>Akshay Saini</span>
-      </div>
-      <div>
-        <img src='subscriptions/dipraj.jpg' alt='dipraj' />
-        <span>Dipraj Jadhav Edits</span>
-      </div>
-      <div>
-        <img src='subscriptions/fireship.jpg' alt='firship' />
-        <span>Fireship</span>
-      </div>
-      <div>
-        <img src='subscriptions/ishan.jpg' alt='ishan' />
-        <span>Ishan Sharma</span>
-      </div>
-      <div>
-        <img src='subscriptions/joma.jpg' alt='joma' />
-        <span>Joma Tech</span>
-      </div>
-      { more && 
-      <>
-        <div>
-          <img src='subscriptions/keerti.jpg' alt='keerti' />
-          <span>Keerti Purswani</span>
-        </div>
-        <div>
-          <img src='subscriptions/kunal.jpg' alt='kunal' />
-          <span>Kunal Kushwaha</span>
-        </div>
-        <div>
-          <img src='subscriptions/netninja.jpg' alt='netninjs' />
-          <span>The Net Ninja</span>
-        </div>
-        <div>
-          <img src='subscriptions/shantanu.jpg' alt='shantanu' />
-          <span>Shantanu Kumar</span>
-        </div>
-        <div>
-          <img src='subscriptions/sharan.jpg' alt='sharan' />
-          <span>Finance With Sharan</span>
-        </div>
-        <div>
-          <img src='subscriptions/tanay.jpg' alt='tanay' />
-          <span>Tanay Pratap</span>
-        </div>
-      </>
-      }
-      <div onClick={() => setMore(!more)}>
-        <img src={more ? `icons/showless.svg` : 'icons/showmore.svg'} alt='showmore' />
-        <span>Show {more ? 'fewer' : 'more'}</span>
-      </div>
+      {title && <p>{title}</p>}
+      { list.visible.map((data, index) => {
+        return(
+          <div key={index}>
+            <img src={data.thumbnail} alt={data.name} />
+            <span>{data.name}</span>
+          </div>
+        )
+      })}
+      { list?.hidden && list.hidden.length > 0 && (
+        <>
+          { more && 
+          <>
+            { list.hidden.map((data, index) => {
+              return(
+                <div key={index}>
+                  <img src={data.thumbnail} alt={data.name} />
+                  <span>{data.name}</span>
+                </div>
+              )
+            })}
+          </>
+          }
+          <div onClick={() => setMore(!more)}>
+            <img src={more ? `icons/showless.svg` : 'icons/showmore.svg'} alt='showmore' />
+            <span>Show {more ? 'fewer' : 'more'}</span>
+          </div>
+        </>
+      )}
     </div>
   )
 }
